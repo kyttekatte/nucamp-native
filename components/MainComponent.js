cf51409c6a31981039a9f191d200a2c4d116d86b
Kytte Burke <gameorg@gmail.com>
2020-10-03 12:23:31 -0400

    Workshop Wk 4

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -321,12 +321,7 @@ class Main extends Component {
         this.props.fetchPromotions();
         this.props.fetchPartners();
 
-        NetInfo.fetch().then(connectionInfo => {
-            (Platform.OS === 'ios') ?
-                Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-                : ToastAndroid.show('Initial Network Connectivity Type: ' +
-                    connectionInfo.type, ToastAndroid.LONG);
-        });
+        this.showNetInfo();
 
         this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
             this.handleConnectivityChange(connectionInfo);
@@ -336,6 +331,14 @@ class Main extends Component {
     componentWillUnmount() {
         this.unsubscribeNetInfo();
     }
+
+    async showNetInfo() {
+        let connectionInfo = await NetInfo.fetch();
+        (Platform.OS === 'ios') ?
+                Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+                : ToastAndroid.show('Initial Network Connectivity Type: ' +
+                    connectionInfo.type, ToastAndroid.LONG);
+    } 
     
     handleConnectivityChange = connectionInfo => {
         let connectionMsg = 'You are now connected to an active network.';
